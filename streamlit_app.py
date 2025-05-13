5884616bf865379a91d334d0700df227a4e2986e
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2025-05-13 17:01:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,9 @@ import datetime
 # Load predefined stock lists
 @st.cache_data
 def load_ticker_list(option):
+    if option == "S&P/TSX":
+        ticker_list = ['^GSPTSE']
     if option == "Canadian Stocks":
-        ticker_list = ['RY.TO', 'TD.to']
         return pd.read_csv("canadian_stocks.csv")["Ticker"].dropna().unique().tolist()
     elif option == "US Stocks":
         ticker_list = ['AAPL', 'HD', 'MSFT']
@@ -31,7 +32,7 @@ def fetch_data(tickers, start_date, end_date):
 st.title("Stock Data Downloader")
 
 # Let user choose the market
-market_choice = st.selectbox("Select Market", ["Canadian Stocks", "US Stocks"])
+market_choice = st.selectbox("Select Market", ["S&P/TSX", "Canadian Stocks", "US Stocks"])
 
 # Load tickers based on choice
 tickers = load_ticker_list(market_choice)
