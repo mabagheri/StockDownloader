d807201a42ac862c3d4370cfcdcf65542c83ba5b
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2025-08-24 23:56:52 -0400

    Update streamlit_app.py
    Added progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,16 +15,27 @@ def load_ticker_list(option):
         ticker_list = ['AAPL', 'HD', 'MSFT']
         return ticker_list # pd.read_csv("us_stocks.csv")["Ticker"].dropna().unique().tolist()
     return []
+    
 # Function to fetch data
 def fetch_data(tickers, start_date, end_date):
     data_dict = {}
-    for ticker in tickers:
+    progress_bar = st.progress(0)
+    status_text = st.empty()
+
+    for i, ticker in enumerate(tickers, start=1):
         try:
+            status_text.text(f"Downloading {ticker} ({i}/{len(tickers)}) ...")
             df = yf.download(ticker, start=start_date, end=end_date)
             if not df.empty:
                 data_dict[ticker] = df
+        
         except Exception as e:
             st.error(f"Error fetching data for {ticker}: {e}")
+
+        # Update progress bar
+        progress_bar.progress(i / len(tickers))
+        
+    status_text.text("✅ Download complete!")
     return data_dict
 st.title("Stock Data Downloader")
 # Let user choose the market
