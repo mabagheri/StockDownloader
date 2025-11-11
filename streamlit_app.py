62a7fb3d9001f20a2fd3845ff70fa7ac7c26437a
Mohammad Ali Bagheri <ma.bagheri@gmail.com>
2025-11-11 14:19:54 -0500

    Comment out DataFrame display in streamlit_app.py
    Comment out DataFrame display lines to reduce output.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,11 +29,11 @@ def fetch_data(tickers, start_date, end_date):
         try:
             status_text.text(f"Downloading {ticker} ({i}/{len(tickers)}) ...")
             df = yf.download(ticker, start=start_date, end=end_date)
-            st.write(df.head(5))
+            # st.write(df.head(5))
             df = df.iloc[:, :].reset_index(drop=False)
-            st.write(df.head(5))
+            # st.write(df.head(5))
             df.columns = ["Date", "Close", "High", "Low", "Open", "Volume"]
-            st.write(df.head(5))
+            # st.write(df.head(5))
             df[["Close", "High", "Low", "Open"]] = df[["Close", "High", "Low", "Open"]].round(2)
             if not df.empty:
                 data_dict[ticker] = df
